e93555cdc2f20c608a3a178573cfa0536006ff03
yeqing <tikazyq@gmail.com>
2015-03-07 00:04:29 +0000

    refactoring

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup(
     name='google-analytics-api-wrapper',
-    version='0.1.1',
+    version='0.1.2.1',
     packages=['analytics_query'],
     url='https://github.com/tikazyq/google-analytics-api-wrapper',
-    license=open('LICENSE').read(),
+    download_url='https://github.com/tikazyq/google-analytics-api-wrapper/0.1.1',
+    license='http://opensource.org/licenses/MIT',
     author='Yeqing Zhang',
     author_email='tikazyq@gmail.com',
     description='The Goolge Analytics wrapper is a convenient tool to extract data from GA via API. It is especially '
-                'useful when the user has many GA profiles / web properties.'
+                'useful when the user has many GA profiles / web properties.',
+    keywords=['google-analytics', 'ga', 'pandas', 'dataframe', 'api']
 )
